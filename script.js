c229c001d7aae7d8eca2f66743eb799042656489
christophe <christo_hurley@hotmail.com>
2022-07-10 18:09:46 -0400

    plus and minus operations working

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
 
 zeroTotal = "0";
 
-let currentNumber = "";
+currentTotal = 0;
+total = 0;
+numberArray = [];
 
+
+operationArray = [];
+
+// for (let i = 0; i < operationArray.length; i++) {
+//     numberArray.push(Number(operationArray[i]))
+// }
+
+opererandArray = [];
+
+currentNumber = "";
+
+zeroTotal = "0";
 const display = document.querySelector("#numDisplay");
 display.classList.add('content');
 display.textContent = zeroTotal;
@@ -13,31 +27,60 @@ const deleteOps = document.querySelector("#delete");
 deleteOps.onclick = function () {
 
     if (currentNumber.length > 1) {
-
+        operationArray.pop()
+        numberArray.pop()
         currentNumber = currentNumber.slice(0, currentNumber.length - 1);
         display.textContent = currentNumber;
-    } else {     
-             currentNumber = ""
-            zeroTotal = "0";
-            display.textContent = zeroTotal;
-        }
+    } else if (currentNumber.length == 0){
+        currentNumber = ""
+        zeroTotal = "0";
+        display.textContent = zeroTotal;
+        operationArray.push("+");
+    } else if(display.textContent == currentTotal) {
+        return;
     }
+}
 
 
 const clear = document.querySelector("#clear");
 clear.onclick = function () {
     delete zeroTotal;
-    currentNumber = ""
+    currentNumber = "";
     zeroTotal = "0";
+
     display.textContent = zeroTotal;
+
+    operationArray = [];
+    numberArray = [];
+
+    currentTotal = 0;
+    total = 0;
 }
 
 
 const parenteses = document.querySelector("#parenteses");
 const plusMinus = document.querySelector("#plusMoins");
 const perc = document.querySelector("#perc");
-const division = document.querySelector("#division");
 
+// perc.onclick = function () {
+//     deleteOps.onclick = function () {
+//         currentNumberslice(0, currentNumber.length - 1);
+//             display.textContent = currentNumber;
+//         } else {
+//             ;
+//             currentNumber = ""
+//             zeroTotal = "0";
+//             display.textContent = zeroTotal;
+//             operationArray.push(currentNumber);
+//             operationArray.push("+");
+//             var percNumber = Number(currentNumber) * 100;
+//             display.textContent = percNumber.toString() + "%";
+//         }
+
+//     var percNumber = Number(currentNumber) * 100;
+//     display.textContent = percNumber.toString() + "%";
+
+// }
 
 const seven = document.querySelector("#seven");
 seven.onclick = function () {
@@ -78,13 +121,14 @@ six.onclick = function () {
     display.textContent = currentNumber;
 }
 
-
 const minus = document.querySelector("#minus");
-
-
-
-
-
+minus.onclick = function () {
+    numberArray.push(Number(currentNumber));
+    operationArray.push(currentNumber);
+    opererandArray.push("-");
+    console.log(operationArray);
+    currentNumber = "";
+}
 
 const one = document.querySelector("#one");
 one.onclick = function () {
@@ -103,12 +147,18 @@ three.onclick = function () {
     display.textContent = currentNumber;
 }
 
+var operations = 0;
 
-// const plus = document.querySelector("#plus");
-// plus.onclick = function () {
+const plus = document.querySelector("#plus");
+plus.onclick = function () {
 
-// }
 
+    numberArray.push(Number(currentNumber));
+    operationArray.push(currentNumber);
+    opererandArray.push("+");
+    console.log(operationArray);
+    currentNumber = "";
+}
 
 
 const zero = document.querySelector("#zero");
@@ -116,6 +166,51 @@ zero.onclick = function () {
     currentNumber += "0";
     display.textContent = currentNumber;
 }
+
+
 const point = document.querySelector("#point");
+point.onclick = function () {
+
+    if (currentNumber.includes(".")) {
+        display.textContent = currentNumber;
+    } else {
+        currentNumber += "."
+        display.textContent = currentNumber;
+    }
+}
+
+
 const exp = document.querySelector("#exp");
-const equal = document.querySelector("#equal");
+
+
+const equal = document.querySelector("#equal")
+
+equal.onclick = function operate () {
+    numberArray.push(Number(currentNumber));
+
+    if (opererandArray.slice(opererandArray.length-1,opererandArray.length)[0] == '+') {
+
+        if (currentTotal == 0) {
+            total = numberArray.slice(numberArray.length - 2, numberArray.length - 1)[0] + numberArray.slice(numberArray.length - 1, numberArray.length)[0];
+            currentTotal += total;
+            display.textContent = currentTotal;
+        } else {
+            currentTotal = currentTotal + numberArray.slice(numberArray.length - 1, numberArray.length)[0];
+            display.textContent = currentTotal
+        }
+    }
+
+    if (opererandArray.slice(opererandArray.length-1,opererandArray.length)[0]== '-') {
+
+        if (currentTotal == 0) {
+            total = numberArray.slice(numberArray.length - 2, numberArray.length-1)[0] - numberArray.slice(numberArray.length - 1, numberArray.length)[0];
+            currentTotal = total;
+            display.textContent = numberArray.slice(numberArray.length - 2, numberArray.length-1)[0].toString() + " - " + numberArray.slice(numberArray.length - 1, numberArray.length)[0].toString() + " = "  + currentTotal.toString();
+        } else {
+            array = [];
+            array.push(currentTotal);
+            currentTotal = currentTotal - numberArray.slice(numberArray.length - 1, numberArray.length)[0];
+            display.textContent = array[0].toString() + " - " +    numberArray.slice(numberArray.length - 1, numberArray.length)[0].toString()     + " = " + currentTotal.toString()
+        }
+    }
+}

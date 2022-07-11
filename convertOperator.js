e47459dd1747309c6051c7acb3da67c2ddc0072d
Christophe Tremblay <tremblay717@gmail.com>
2022-07-11 17:14:55 -0400

    Update convertOperator.js
    removed leng variable and replace it by  numberArray.length (line 3)

--- convertOperator.js
+++ convertOperator.js
@@ -1,7 +1,6 @@
-// test function for the calculator in the event we have more than 1 operation to compute. 
-function convertOperatorToMaths(opererandArray, numberArray, leng, total) {
+function convertOperatorToMaths(opererandArray, numberArray,total) {
 
-  for (let i = 0; i < leng - 1; i++) {
+  for (let i = 0; i < numberArray.length - 1; i++) {
     if (opererandArray.slice(i, i + 1) == "+") {
       if (total.length == 0) {
         let currentEquation = numberArray.slice(i, i + 1)[0] + numberArray.slice(i + 1, i + 2)[0];
@@ -51,9 +50,9 @@ function convertOperatorToMaths(opererandArray, numberArray, leng, total) {
   }
 }
 
-let numberArray = [12, 7, 5, 3];
-let opererandArray = ["+", "-", "x"];
+let numberArray = [12, 7, 5, 3,7];
+let opererandArray = ["+", "-", "x","/"];
 let total = [];
-var leng = numberArray.length;
 
-convertOperatorToMaths(opererandArray, numberArray, leng, total);
+
+convertOperatorToMaths(opererandArray, numberArray,total);

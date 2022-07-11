381073147296f3a143b8c84e47081117f935ed99
Christophe Tremblay <tremblay717@gmail.com>
2022-07-11 17:28:06 -0400

    fixed missing semi-colons

--- convertOperator.js
+++ convertOperator.js
@@ -6,8 +6,8 @@ function convertOperatorToMaths(opererandArray, numberArray,total) {
         let currentEquation = numberArray.slice(i, i + 1)[0] + numberArray.slice(i + 1, i + 2)[0];
         total.push(currentEquation);
       } else {
-        let currentEquation = total.slice(total.length - 1, total.length)[0] + numberArray.slice(i + 1, i + 2)[0]
-        total.push(currentEquation)
+        let currentEquation = total.slice(total.length - 1, total.length)[0] + numberArray.slice(i + 1, i + 2)[0];
+        total.push(currentEquation);
       }
     }
     else if (opererandArray.slice(i, i + 1) == "-") {
@@ -15,8 +15,8 @@ function convertOperatorToMaths(opererandArray, numberArray,total) {
         let currentEquation = numberArray.slice(i, i + 1)[0] - numberArray.slice(i + 1, i + 2)[0];
         total.push(currentEquation);
       } else {
-        let currentEquation = total.slice(total.length - 1, total.length)[0] - numberArray.slice(i + 1, i + 2)[0]
-        total.push(currentEquation)
+        let currentEquation = total.slice(total.length - 1, total.length)[0] - numberArray.slice(i + 1, i + 2)[0];
+        total.push(currentEquation);
       }
     }
     else if (opererandArray.slice(i, i + 1) == "/") {
@@ -24,8 +24,8 @@ function convertOperatorToMaths(opererandArray, numberArray,total) {
         let currentEquation = numberArray.slice(i, i + 1)[0] / numberArray.slice(i + 1, i + 2)[0];
         total.push(currentEquation);
       } else {
-        let currentEquation = total.slice(total.length - 1, total.length)[0] / numberArray.slice(i + 1, i + 2)[0]
-        total.push(currentEquation)
+        let currentEquation = total.slice(total.length - 1, total.length)[0] / numberArray.slice(i + 1, i + 2)[0];
+        total.push(currentEquation);
       }
     }
     else if (opererandArray.slice(i, i + 1) == "x") {
@@ -33,8 +33,8 @@ function convertOperatorToMaths(opererandArray, numberArray,total) {
         let currentEquation = numberArray.slice(i, i + 1)[0] * numberArray.slice(i + 1, i + 2)[0];
         total.push(currentEquation);
       } else {
-        let currentEquation = total.slice(total.length - 1, total.length)[0] * numberArray.slice(i + 1, i + 2)[0]
-        total.push(currentEquation)
+        let currentEquation = total.slice(total.length - 1, total.length)[0] * numberArray.slice(i + 1, i + 2)[0];
+        total.push(currentEquation);
       }
     }
     else if (opererandArray.slice(i, i + 1) == "**") {
@@ -42,8 +42,8 @@ function convertOperatorToMaths(opererandArray, numberArray,total) {
         let currentEquation = numberArray.slice(i, i + 1)[0] ** numberArray.slice(i + 1, i + 2)[0];
         total.push(currentEquation);
       } else {
-        let currentEquation = total.slice(total.length - 1, total.length)[0] ** numberArray.slice(i + 1, i + 2)[0]
-        total.push(currentEquation)
+        let currentEquation = total.slice(total.length - 1, total.length)[0] ** numberArray.slice(i + 1, i + 2)[0];
+        total.push(currentEquation);
       }
     }
     console.log(total);
